26687acfef14615baf91d5b00d549ba26a3424a7
AadishRahman <aadishhabeeb123@gmail.com>
2023-12-05 00:13:39 +0530

    "Completed Project Setup"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import React from 'react';
 
-function App() {
-  return <div>HELLO WORLD</div>;
-}
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
